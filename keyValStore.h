58b76896ef2339fd22b512eaca99d6b748c7ce43
Erik <130369758+Erikgau@users.noreply.github.com>
2024-06-20 16:51:32 +0200

    Update keyValStore.h

--- keyValStore.h
+++ keyValStore.h
@@ -1,22 +1,31 @@
-//
-// Created by Erik on 09.05.2024.
-//
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdlib.h>
 
 #ifndef MEILENSTEIN_1_KEYVALSTORE_H
 #define MEILENSTEIN_1_KEYVALSTORE_H
 
 #define BUFFER_SIZE 1024
+#define SHM_KEY 0x1234 // Shared memory key
 
 struct KeyValue {
     char key[256];
     char value[256];
 };
 
-extern struct KeyValue keyValueStore[100];
-extern int numKeyValues;
+extern int shm_id;
+extern struct KeyValue* keyValueStore;
+extern int* numKeyValues;
 
 int put(char* key, char* value);
 int get(char* key, char* res);
 int del(char* key);
+
+void initialize_shared_memory();
+void detach_shared_memory();
+void cleanup_shared_memory();
 
 #endif //MEILENSTEIN_1_KEYVALSTORE_H
