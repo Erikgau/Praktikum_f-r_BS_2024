8b313e99fa3afe5dd882ee24a056ea4c9517e8c8
Erik <130369758+Erikgau@users.noreply.github.com>
2024-06-20 16:53:13 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,11 @@
+#include "keyValStore.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+#include "sub.h"
